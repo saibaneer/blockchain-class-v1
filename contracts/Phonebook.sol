ec4f5dd1265d1f233534bc820d3d7f3bcb43caed
Toba Ajiboye <gbenga.aj66@gmail.com>
2025-08-25 18:35:58 +0100

    first commit

---
+++ contracts/Phonebook.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Phonebook {
+    // Add contact struct
+    struct Contact {
+        string firstName;
+        string lastName;
+        uint8 age;
+        uint256 bankBalance;
+    }
+
+    // mapping(string => Contact) private stringToContact;
+    mapping(bytes32 => Contact) private contacts;
+    mapping(uint8 => Contact) private ageToContact;
+
+    // Add a contact
+    function addContact(string memory _fName, string memory _lName, uint8 age, uint256 _startingBalance) external {
+        bytes32 hashedContact = hashContact(_fName, _lName, age);
+        contacts[hashedContact] = Contact(_fName, _lName, age, _startingBalance);
+        ageToContact[age] = Contact(_fName, _lName, age, _startingBalance);
+    }
+
+    // Update a contact info
+    function addBalance(bytes32 _hashedContact, uint256 _amount) external  {
+        contacts[_hashedContact].bankBalance += _amount;
+        ageToContact[contacts[_hashedContact].age].bankBalance += _amount;
+    }
+    function reduceBalance(bytes32 _hashedContact, uint256 _amount) external  {
+        contacts[_hashedContact].bankBalance -= _amount;
+        uint8 age = contacts[_hashedContact].age;
+        ageToContact[age].bankBalance -= _amount;
+     }
+
+    // Read/View a contract
+    function viewContact(string memory _fName, string memory _lName, uint8 age) external view returns(Contact memory)  {
+        bytes32 hashedContact = hashContact(_fName, _lName, age);
+        return contacts[hashedContact];
+    }
+
+    function viewContactViaNumber(uint8 _age) external view returns(Contact memory)  {
+        return ageToContact[_age];
+    }
+
+
+    // Delete a contact
+    function deleteContact() external {}
+
+    function hashContact(string memory _fName, string memory _lName, uint8 age) public pure returns(bytes32) {
+        return keccak256(abi.encode(_fName, _lName, age));
+    }
+}
+
+
+// 0x22a3480f3d671a29efe2d638b5c3220ed945089c205e243ccadd2817303c7b8e
+
